cf615f5023f336eb58a4fabcbc40d204558c3c9d
Brittany Klose <brittany.klose@yahoo.com>
2025-04-24 15:32:03 -0400

    Connection made!

--- SEFPDentist25official/src/dentist.java
+++ SEFPDentist25official/src/dentist.java
@@ -1,6 +1,24 @@
+import java.sql.*;
+
 public class dentist {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Please tell me this will work or i'll cry.");
+    static final String DB_URL="jdbc:mysql://localhost/dentistsdb";
+    static final String USER = "root";
+    static final String pas = "";
+    static final String QUERY = "SELECT first_name, last_name FROM patients";
+    public static void main(String[] args) {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, pas);
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY);) {
+                while (rs.next()){
+                    System.out.print("FirstName: " + rs.getString("first_name"));
+                    System.out.print(", LastName: " + rs.getString("last_name"));
+                }
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+
     }
+         
+    
 }
