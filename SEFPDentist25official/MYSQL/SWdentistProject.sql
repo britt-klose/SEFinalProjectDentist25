b7fe4d957b1ebba66f33f61c7e00bdc7cfff755c
Brittany Klose <brittany.klose@yahoo.com>
2025-04-24 14:45:11 -0400

    Add files via upload

---
+++ SEFPDentist25official/MYSQL/SWdentistProject.sql
@@ -0,0 +1,71 @@
+set FOREIGN_KEY_CHECKS=0;
+set SQL_MODE = 'NO_ZERO_DATE';
+
+-- Initialize Database
+drop database if exists dentistsdb;
+create database dentistsdb;
+use dentistsdb;
+
+-- Create Patients Table
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone  varchar(200),
+    email varchar(50) not null,
+    date_of_birth DATE NOT NULL,
+    address varchar(100) not null,
+    city varchar(100),
+    state  varchar(100),
+    zipcode  varchar(100),
+    gender  varchar(100),
+
+    constraint gender_constraint check (gender in
+    ('F', 'M', 'O'))
+);
+
+-- Create Procedures Table
+CREATE TABLE procedures (
+    procedure_id INT PRIMARY KEY AUTO_INCREMENT,
+    procedure_name VARCHAR(100) NOT NULL
+);
+
+-- Create Patient History Table
+drop table if exists patient_history;
+CREATE TABLE patient_history (
+    record_id INT primary key,
+    patient_id INT NOT NULL,
+    procedure_id INT NOT NULL,
+    procedure_date DATE NOT NULL,
+    dentist VARCHAR(100) NOT NULL,
+    assistant VARCHAR(100) NOT NULL,
+    notes  varchar(200),
+
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id), 
+    FOREIGN KEY (procedure_id) REFERENCES procedures(procedure_id), 
+    
+    constraint dentist_constraint check(
+    dentist in
+    ('Brittany Klose', 'John Smith', 'Samuel Duro-Aina', 'John Smith ')),
+    
+    constraint assistant_constraint check(
+    assistant in
+    ('Sam Winchester', 'Nathaniel Bacon', 'Nicolas Jackson', 'Mark Grayson', 'Nolan Edward', 'Harry Potter'))
+);
+
+select * from patients;
+
+select * from procedures;
+
+select * from patient_history;
+
+SELECT DISTINCT dentist FROM patient_history;
+
+SELECT DISTINCT assistant FROM patient_history;
+
+
+
+
+
+
+
---
+++ SEFPDentist25official/src/dentist.java
@@ -0,0 +1,6 @@
+public class dentist {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Please tell me this will work or i'll cry.");
+    }
+}
