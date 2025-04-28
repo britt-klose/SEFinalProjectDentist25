1785200f3fd08e3ceb15a8572001d95c4ee61476
Brittany Klose <brittany.klose@yahoo.com>
2025-04-27 20:52:57 -0400

    console code finished

--- SEFPDentist25official/src/dentist.java
+++ SEFPDentist25official/src/dentist.java
@@ -7,32 +7,34 @@ public class dentist {
     static final String DB_URL="jdbc:mysql://localhost/dentistsdb";
     static final String USER = "root";
     static final String pas = "";
-    //static String strSQL="select patient_id, first_name, last_name, date_of_birth from patients order by last_name";
-    static String strSQL;
     public static void main(String[] args) {
-        try(Connection conn = DriverManager.getConnection(DB_URL, USER, pas);
+        try
+		{
+			boolean keepRunning= true;
+			Connection conn = DriverManager.getConnection(DB_URL, USER, pas);
             Statement stmt = conn.createStatement();
+			String strSQL="select procedure_name from procedures";
         
-             ResultSet rs = stmt.executeQuery(strSQL);) {
-            //     while (rs.next())
-            //     {
-            //         System.out.print(rs.getInt("patient_id") + " ");
-            //         System.out.print(rs.getString("first_name") + " ");
-            //         System.out.print(rs.getString("last_name") + " ");
-            //         System.out.print(rs.getString("date_of_birth")+ ", ");
-                    
-            //     }
+             ResultSet rs = stmt.executeQuery(strSQL); 
+                while (rs.next())
+                {
+					System.out.println(rs.getString("procedure_name") + " ");
+					System.out.println(" ");
+                }
+				System.out.println("\nHello Welcome to Clean Dental Clinic!");
 
+			while(keepRunning =true){
                 Scanner input=new Scanner(System.in);
-				System.out.print("Hell Welcome to Clean Dental Clinic!");
-				System.out.print("\nSelect '1' for procedures, '2' for patients, '3' for staff");
+				displayMenu();
+				//System.out.println("\nMain Menu: ");
+				//System.out.println("\nSelect '1' for procedures, '2' for patients, '3' for staff: ");
 				String strMenuOption=String.valueOf(input.next().charAt(0));
 
 				switch(strMenuOption)
 				{
 					case "1": //Procedures
 					System.out.println("===PROCEDURES===");
-					System.out.print("\n Type 'v' to view all procedures, 's' to search, 'a' to add, 'u' to update, 'd' to delete: ");
+					System.out.println("\nType 'v' to view all procedures, 's' to search, 'a' to add, 'u' to update, 'd' to delete: ");
 					String procedureOption=String.valueOf(input.next().charAt(0));
 
 					switch (procedureOption)
@@ -41,7 +43,16 @@ public class dentist {
 						case "v":
 							System.out.println("===PROCEDURE LIST===");
 							strSQL="select * from procedures order by procedure_name";
-							System.out.println(strSQL);
+							rs = stmt.executeQuery(strSQL); 
+							while (rs.next())
+							{
+								System.out.print(rs.getInt("procedure_id") + " ");
+								System.out.println(rs.getString("procedure_name") + " ");
+								System.out.println(rs.getString("description") + " ");
+								System.out.println("Price: " + rs.getString("start_price") + " ");
+								System.out.println("Requires Anesthesia: " + rs.getString("requires_anestesia"));
+								System.out.println(" ");
+							}	
 							break;
 
 						case "s":
@@ -50,7 +61,16 @@ public class dentist {
 							String strProcedureName=input.next();
 							
 							strSQL="select * from procedures where procedure_name='" + strProcedureName + "'";
-							System.out.println(strSQL);
+							rs = stmt.executeQuery(strSQL); 
+							while (rs.next())
+							{
+								System.out.print(rs.getInt("procedure_id") + " ");
+								System.out.println(rs.getString("procedure_name") + " ");
+								System.out.println(rs.getString("description") + " ");
+								System.out.println("Price: " + rs.getString("start_price") + " ");
+								System.out.println("Requires Anesthesia: " + rs.getString("requires_anestesia"));
+								System.out.println(" ");
+							}
 							break;
 							
 						case "a":
@@ -134,7 +154,7 @@ public class dentist {
 
 					case "2": //Patients
 					System.out.println("===PATIENTS===");
-					System.out.print("\nType 'a' to add, 'u' to update, 'd' to delete: ");
+					System.out.print("\nType 'a' to add, 'u' to update, 'd' to delete, 's' to search a patient's history: ");
 					String patientChoice=String.valueOf(input.next().charAt(0));
 					switch (patientChoice)
 					{
@@ -210,6 +230,25 @@ public class dentist {
 							System.out.println(strSQL);
 							stmt.executeUpdate(strSQL);
 							break;
+
+						case "s":
+							System.out.println("===SEARCH===");
+							System.out.print("enter patient id: ");
+							int pId=input.nextInt();
+							
+							strSQL="select procedure_name, procedure_date, dentist, assistant, notes from patient_history inner join procedures on patient_history.procedure_id=procedures.procedure_id where patient_id='" + pId + "'";
+							rs = stmt.executeQuery(strSQL); 
+							while (rs.next())
+							{
+								System.out.print("Data for patient: " + pId);
+								System.out.println(rs.getString("procedure_name") + " ");
+								System.out.println(rs.getString("procedure_date") + " ");
+								System.out.println(rs.getString("dentist") + " ");
+								System.out.println(rs.getString("assistant") + " ");
+								System.out.println(rs.getString("notes"));
+								System.out.println(" ");
+							}
+							break;
 							
 					}
 					break;
@@ -226,13 +265,23 @@ public class dentist {
 						case "d":
 							System.out.println("===DENTIST LIST===");
 							strSQL="select distinct dentist from patient_history order by dentist asc";
-							System.out.println(strSQL);
+							rs = stmt.executeQuery(strSQL); 
+							while (rs.next())
+							{
+								System.out.print(rs.getString("dentist"));
+								System.out.println(" ");
+							}
 							break;
 						
 						case "a":
 							System.out.println("===ASSISTANT LIST===");
 							strSQL="select distinct assistant from patient_history order by assistant asc";
-							System.out.println(strSQL);
+							rs = stmt.executeQuery(strSQL); 
+							while (rs.next())
+							{
+								System.out.print(rs.getString("assistant"));
+								System.out.println(" ");
+							}
 							break;
 
 						case "s":
@@ -240,12 +289,30 @@ public class dentist {
 							System.out.println("Enter dentist name: ");
 							String strDentistName=input.next();
 							System.out.println("Loading " + strDentistName + "'s history ....");
-							strSQL="select concat(patient.first_name, ' ', patient.last_name) as 'patient', procedure_name, procedure_date, assistant from patient_history inner join patients on patient_history.patient_id=patients.patient_id inner join procedures on patient_history.procedure_id=procedures.procedure_id where dentist='" + strDentistName + "'";
-							System.out.println(strSQL);
+							strSQL="select concat(patients.first_name, ' ', patients.last_name) as 'patient', procedure_name, procedure_date, assistant from patient_history inner join patients on patient_history.patient_id=patients.patient_id inner join procedures on patient_history.procedure_id=procedures.procedure_id where dentist='" + strDentistName + "'";
+							rs = stmt.executeQuery(strSQL); 
+							while (rs.next())
+							{
+								System.out.print(rs.getString("first_name") + ",");
+								System.out.print(rs.getString("last_name") + ",");
+								System.out.println(rs.getString("procedure_name") + " ");
+								System.out.println(rs.getString("procedure_date") + " ");
+								System.out.print(rs.getString("assistant"));
+								System.out.println(" ");
+							}
 							break;
 					}
 
-				}
+				}  if (keepRunning) {
+                    System.out.println("Do you want to perform another function? 'y' or 'n': ");
+                    String response = input.next();
+                    if (!response.equalsIgnoreCase("y")) {
+                        keepRunning = false;
+                        System.out.println("Goodbye!");
+                    }
+                }
+			}
+				
 			rs.close();
 			conn.close();
 
@@ -254,6 +321,11 @@ public class dentist {
             catch (Exception e) {e.printStackTrace();}	 // was SQLException
 
     }
+	public static void displayMenu()
+		{
+			System.out.println("===MAIN MENU===");
+			System.out.println("\nSelect '1' for procedures, '2' for patients, '3' for staff: ");
+		}
          
     
 }
